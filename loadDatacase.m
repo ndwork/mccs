8da466ac5db8b51a0373d5a158246a27376fba8c
Nicholas Dwork <ndwork@hotmail.com>
2019-11-26 16:03:43 -0800

    Adding brain data into load function.

--- loadDatacase.m
+++ loadDatacase.m
@@ -1,6 +1,15 @@
 
-function data = loadDatacase( datacase )
-  % Returns data hybrid state
+function [data,noiseCoords] = loadDatacase( datacase )
+  % Returns data in hybrid state
+  %
+  % Index:
+  % datacase - integer specifying datacase
+  %
+  % Ouptuts:
+  % data - array of data in hybrid state
+  % noiseCoords - [ minX minY maxX maxY ] specifying noise region in data
+  %
+  % Written by Nicholas Dwork, Copyright 2019
 
   dataDir = '/Users/nicholasdwork/.DellEMS/Vols/disk2s1';
 
@@ -8,21 +17,65 @@ function data = loadDatacase( datacase )
 
     case 1
       data = readOldMriDataOrgData( [ dataDir, '/mriData.Org/P14/kspace' ] );
-      data = ifft( data, [], 3 );
+      data = ifftc( data, [], 3 );
       data = data( :, :, 5:10:end, : );
       data = data( :, :, 4, : );
+      noiseCoords = [ 1 1 41 76 ];
 
     case 2
       data = readOldMriDataOrgData( [ dataDir, '/mriData.Org/P17/kspace' ] );
-      data = ifft( data, [], 3 );
+      data = ifftc( data, [], 3 );
       data = data( :, :, 5:10:end, : );
       data = data( :, :, 4, : );
+      noiseCoords = [ 1 1 56 72 ];
 
     case 3
-      [data,header] = read_MR_rawdata( [ dataDir, '/mccs/P54784.7' ] );   %#ok<ASGLU>
+      load( './ESPIRIT/data/brain_8ch.mat', 'DATA' );
+      data = permute( DATA, [1 2 4 3] );
+      noiseCoords = [ 1 1 35 32 ];
+      %kcf = 0.008;
+      %lambda_xs = 1d-12 * ones( numel( sampleFractions ), 1 );
+      %lambda_ss = 1d-11 * ones( numel( sampleFractions ), 1 );
+
+    case 4
+      load( './ESPIRIT/data/brain_32ch.mat', 'DATA' );
+      data = permute( DATA, [1 2 4 3] );
+      noiseCoords = [ 1 1 35 32 ];
+      %kcf = 0.008;
+      %lambda_xs = 1d-12 * ones( numel( sampleFractions ), 1 );
+      %lambda_ss = 1d-11 * ones( numel( sampleFractions ), 1 );
+
+    case 5
+      % FOV 25.6 cm, 256 x 256 (1mm in plane, 1.5mm thick slices)
+      [data,header] = read_MR_rawdata( [ dataDir, '/brain3d/P73216.7' ] );   %#ok<ASGLU>
       data = squeeze( data );
-      data = ifft( ifftshift( data, 3 ), [], 3 );
-      data = squeeze( data(:,:,62,:) );
+      data = ifftc( data, [], 3 );
+      data = data(:,:,64,:);
+      noiseCoords = [ 1 1 50 50 ];
+
+    case 6
+      % FOV 25.6 cm, 256 x 256 (1mm in plane, 1mm thick slices)
+      [data,header] = read_MR_rawdata( [ dataDir, '/brain3d/P73728.7' ] );   %#ok<ASGLU>
+      data = squeeze( data );
+      data = ifftc( data, [], 3 );
+      data = data(:,:,64,:);
+      noiseCoords = [ 1 1 50 50 ];
+
+    case 7
+      % FOV 20.5, 256 x 256 (0.8mm in plane, 0.8mm thick slices)
+      [data,header] = read_MR_rawdata( [ dataDir, '/brain3d/P74240.7' ] );   %#ok<ASGLU>
+      data = squeeze( data );
+      data = ifftc( data, [], 3 );
+      data = data(:,:,64,:);
+      noiseCoords = [ 1 1 45 45 ];
+
+    case 8
+      % FOV 20 cm, 400 x 400 (0.5mm in plane, 0.5mm thick slices)
+      [data,header] = read_MR_rawdata( [ dataDir, '/brain3d/P74752.7' ] );   %#ok<ASGLU>
+      data = squeeze( data );
+      data = ifftc( data, [], 3 );
+      data = data(:,:,64,:);
+      noiseCoords = [ 1 1 70 70 ];
 
     otherwise
       error( 'This datacase doesn''t exist' );
