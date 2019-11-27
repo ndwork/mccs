b55ff1c89c83bc0d82dc41ec652108fce632cb1f
Nicholas Dwork <ndwork@hotmail.com>
2019-11-26 16:04:52 -0800

    Now penalizing higher frequencies rather than setting them to 0.

--- mri_mccsRecon.m
+++ mri_mccsRecon.m
@@ -1,9 +1,15 @@
 
-function [recon,senseMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, varargin )
-  % recon = mri_mccsRecon( kData, lambda [, 'maxOuterIter', maxOuterIter ] )
+function [recon,senseMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, lambda_h, varargin )
+  % recon = mri_mccsRecon( kData, lambda [, 'maxOuterIter', maxOuterIter, 'noiseCov', noiseCov ] )
   %
   % Inputs:
   % kData is an array of size ( Ny, Nx, nSlices, nCoils ) of kSpace values
+  % lambda_x - regularization parameter for compressed sensing reconstruction
+  % lambda_s - regularization parameter for determining sensitivity maps
+  %
+  % Optional Inputs:
+  % maxOuterIter - scalar specifying the number of outer iterations
+  % noiseCov - a 2D array of size nCoils x nCoils specifying the noise covariance
   %
   % Outputs:
   % recon - the final reconstructed volue
@@ -23,7 +29,8 @@ function [recon,senseMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, varargin 
   p.addRequired( 'lambda_s', @(x) numel(x) == 1 && x >= 0 );
   p.addParameter( 'doCheckAdjoint', false, @islogical );
   p.addParameter( 'kcf', 0.0035, @(x) x >= 0 && numel(x) == 1 );
-  p.addParameter( 'maxOuterIter', 1000, @ispositive );
+  p.addParameter( 'maxOuterIter', 30, @ispositive );
+  p.addParameter( 'noiseCov', [], @isnumeric );
   p.addParameter( 'outDir', './out', @(x) true );
   p.addParameter( 'range', [], @isnumeric );
   p.addParameter( 'verbose', false, @islogical );
@@ -31,36 +38,46 @@ function [recon,senseMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, varargin 
   doCheckAdjoint = p.Results.doCheckAdjoint;
   kcf = p.Results.kcf;
   maxOuterIter = p.Results.maxOuterIter;
+  noiseCov = p.Results.noiseCov;
   outDir = p.Results.outDir;
   range = p.Results.range;
   verbose = p.Results.verbose;
 
   ssqRecon = mri_ssqRecon( kData );  % (Ny, Nx, nSlices, nCoils )
+  [ Ny, Nx ] = size( ssqRecon );
+  nPix = Ny * Nx;
   recon = ssqRecon;
 
   if verbose == true && ~exist( outDir, 'dir' ), mkdir( outDir ); end
 
   roughMaps = mccs_makeInitialSensitivityMap( kData );
+  %roughMaps = mri_makeSensitivityMaps( kData );
   senseMaps = roughMaps;
 
   if verbose == true
     mapsFig = figure;
     senseReconsFig = figure;
     reconFig = figure;
+    logID = fopen( [outDir, '/mccs.log' ], 'w' );
+    fprintf( logID, 'mdm, mdm-2, niqe, piqe, maxSenseMapMap\n' );
   end
 
   for iter = 1 : maxOuterIter
     disp([ 'Working on iteration ', num2str(iter), ' of ', num2str(maxOuterIter) ]);
 
     % Determine the sensitivity maps
-    senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, ...
-      'initialGuess', senseMaps, 'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, ...
-      'maxIterOpt', 200 );
+    senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, lambda_h, ...
+      'initialGuess', senseMaps, 'noiseCov', noiseCov, 'maxIterOpt', 60, ...
+      'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint );
 
     if verbose == true
       figure( mapsFig );  showImageCube( abs( senseMaps ), 5 );
       if numel( outDir ) > 0
         saveas( mapsFig, [outDir, '/senseMaps_', indx2str(iter,maxOuterIter), '.png'] );
+        save( [outDir, '/mat_senseMaps.mat'], 'senseMaps', 'iter' );
+        if mod( iter, 10 ) == 0
+          save( [outDir, '/mat_senseMaps_', indx2str(iter,maxOuterIter), '.mat'], 'senseMaps' );
+        end
       end
 
       senseRecons = bsxfun( @times, senseMaps, recon );
@@ -71,18 +88,40 @@ function [recon,senseMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, varargin 
     end
 
     % Estimate the reconstructed image
-    recon = mri_csReconFISTA_multiCoilMultiSlice( kData, senseMaps, lambda_x, ...
-      'nIter', 50, 'initialGuess', recon, 'verbose', verbose );
+    recon = mri_csReconFISTA_multiCoilMultiSlice( kData, senseMaps, lambda_x*nPix, ...
+      'nIter', 30, 'initialGuess', recon, 'noiseCov', noiseCov, 'verbose', verbose );
 
+    maxAbsRecon = max( abs( recon(:) ) );
     if verbose == true
       figure( reconFig ); imshowscale( abs(recon), 5, 'range', range );
       titlenice([ 'recon ', num2str(iter) ]);  drawnow;
       if numel( outDir ) > 0
         saveas( reconFig, [outDir, '/recon_', indx2str(iter,maxOuterIter), '.png'] );
-        save( [outDir, '/mat_recon_', indx2str(iter,maxOuterIter), '.mat'], 'recon' );
+        save( [outDir, '/mat_recon.mat'], 'recon', 'iter' );
+        if mod( iter, 10 ) == 0
+          save( [outDir, '/mat_recon_', indx2str(iter,maxOuterIter), '.mat'], 'recon' );
+        end
       end
+
+      % Calculate image statistics
+      maxSenseMapMag = max( abs( senseMaps(:) ) );
+      
+      % Calculate quality scores
+      if maxAbsRecon == 0
+        normAbsRecon = recon;
+      else
+        normAbsRecon = abs( recon ) / max( abs( recon(:) ) );
+      end
+      mdmScore = calcMdmMetric( normAbsRecon );
+      mdmScore2 = calcMdmMetric( 1-normAbsRecon );
+      niqeScore = niqe( normAbsRecon );
+      piqeScore = piqe( normAbsRecon );
+      fprintf( logID, '%f, %f, %f, %f, %f\n', mdmScore, mdmScore2, ...
+        niqeScore, piqeScore, maxSenseMapMag );
     end
+    if maxAbsRecon == 0, return; end
   end
 
 end
+
 
--- run_mri_mccsRecon.m
+++ run_mri_mccsRecon.m
@@ -1,129 +1,194 @@
 
 function run_mri_mccsRecon
-  close all; clear; rng(2);
+  close all; clear; rng(1);
 
-  datacase = 3;
-  doCheckAdjoint = true;
+  %datacases = [ 1 7 3 2 5 8 6 ];
+datacases = 3;
+
+  doCheckAdjoint = false;
   simCase = 1;
   doSimulation = false;
-  lambda_x = 1d-5;
-  lambda_s = 1d-2;
-  labmda_iSENSEnn_x = 1d-3;  % Works well
+  %vdSigma = 0.25;
+vdSigma = 0.3;
+  maxOuterIter = 100;
+  useNoiseCov = true;
+
+  sampleFractions = [ 0.1 0.15 0.2 0.25 0.3 0.4 ];
+
+  %lambda_x = 5d-4;  % 1d-5 works well without noise covariance
+  %lambda_s = 1d-4;
+
+  labmda_iSENSEnn_x = 1d-3;
   labmda_iSENSEnn_s = 1d-2;
   verbose = true;
 
-  sampleFractions = [ 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 ];
-  vdSigma = 75;
 
-  senseMaps = [];
-  if doSimulation == true
-    [ sub_kData, senseMaps ] = loadSimData( simCase );
-  else
-    dataMatFile = [ 'sub_kData_', indx2str(datacase,99), '.mat' ];
-    if exist( dataMatFile, 'file' )
-      sub_kData = [];
-      load( dataMatFile );
+  for datacase = datacases
+    if doSimulation == true
+      [ sub_kData, senseMaps ] = loadSimData( simCase );  %#ok<ASGLU>
     else
-      sub_kData = loadDatacase( datacase );
-      save( dataMatFile, 'sub_kData' );
+      dataMatFile = [ 'sub_kData_', indx2str(datacase,99), '.mat' ];
+      if exist( dataMatFile, 'file' )
+        load( dataMatFile, 'sub_kData', 'noiseCoords' );
+      else
+        [sub_kData,noiseCoords] = loadDatacase( datacase );
+        save( dataMatFile, 'sub_kData', 'noiseCoords' );
+      end
     end
-  end
-  sub_kData = sub_kData ./ max( abs( sub_kData(:) ) );
+    sub_kData = sub_kData ./ max( abs( sub_kData(:) ) );
 
-  mask = mri_makeIntensityMask( sub_kData );
-  fullySampledSenseMapsMatFile = ['fullySampledSenseMaps_', indx2str(datacase,99), '.mat' ];
-  if exist( fullySampledSenseMapsMatFile, 'file' )
-    fullySampledSenseMaps = [];
-    load( fullySampledSenseMapsMatFile );
-  else
-    fullySampledSenseMaps = mri_makeSensitivityMaps( sub_kData, 'mask', mask, 'verbose', verbose );
-    save( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );
-  end
+    if datacase == 3
+      %kcf = 0.008;
+      kcf = 0.015;
+      lambda_xs = 1d-10 * ones( numel( sampleFractions ), 1 );
+      lambda_ss = 1d-9 * ones( numel( sampleFractions ), 1 );
+      lambda_hs = 1d2 * ones( numel( sampleFractions ), 1 );
+    else
+      kcf = 0.005;
+      lambda_xs = 1d-11 * ones( numel( sampleFractions ), 1 );
+      lambda_ss = 1d-3 * ones( numel( sampleFractions ), 1 );
+      lambda_hs = 1d2 * ones( numel( sampleFractions ), 1 );
+    end
 
-  %parfor sampleFractionIndx = 1 : numel( sampleFractions )
-for sampleFractionIndx = 1 : numel( sampleFractions )
-    sampleFraction = sampleFractions( sampleFractionIndx );
-  
-    outDir = [ './out/datacase_', indx2str(datacase,100), '_', num2str(sampleFraction,'%3.2f') ];
-    if ~exist( outDir, 'dir' ), mkdir( outDir ); end
+    datacaseDir = [ './out/datacase_', indx2str(datacase,100) ];
+    if ~exist( datacaseDir, 'dir' ), mkdir( datacaseDir ); end
+    logFile = [ datacaseDir, '/metrics.log' ];
+    logID = fopen( logFile, 'w' );
+    fprintf( logID, 'datacase, sampleFraction, nSamples, type, mdm, mdm-2, niqe, piqe\n' );
+    fclose( logID );
+
+    mask = mri_makeIntensityMask( sub_kData );
+    fullySampledSenseMapsMatFile = ['fullySampledSenseMaps_', indx2str(datacase,99), '.mat' ];
+    if exist( fullySampledSenseMapsMatFile, 'file' )
+      load( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );  %#ok<LOAD>
+    else
+      fullySampledSenseMaps = mri_makeSensitivityMaps( sub_kData, 'mask', mask, 'verbose', verbose );
+      save( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );
+    end
+
+    fftRecon_fullySampled = mri_fftRecon( sub_kData, 'multiSlice', true );
+    if useNoiseCov == true
+      noiseRegions = fftRecon_fullySampled( noiseCoords(2):noiseCoords(4), ...
+        noiseCoords(1):noiseCoords(3), :, : );
+      sNoiseRegions = size( noiseRegions );
+      noiseVecs = reshape( noiseRegions, [ prod(sNoiseRegions(1:3)) sNoiseRegions(4) ] );
+      noiseCov = cov( noiseVecs );
+      noiseCov = noiseCov ./ max( abs( noiseCov(:) ) );
+    else
+      noiseCov = [];
+    end
 
     ssqRecon_fullySampled = mri_ssqRecon( sub_kData, 'multiSlice', true);
     ssqRecon_fullySampledFig = figure;  imshowscale( abs( ssqRecon_fullySampled ), 5 );
-    saveas( ssqRecon_fullySampledFig, [outDir, '/recon_ssqFullySampled.png'] );
+    saveas( ssqRecon_fullySampledFig, [datacaseDir, '/recon_ssqFullySampled.png'] );
     close( ssqRecon_fullySampledFig );
+    printMetrics( logFile, datacase, 1.0, numel(mask), ssqRecon_fullySampled, 'ssq Fully Sampled' );
 
     fullySampledSenseRecons = bsxfun( @times, fullySampledSenseMaps, ssqRecon_fullySampled );
     fullySampledSenseReconFig = figure;  showImageCube( abs(fullySampledSenseRecons), 5 );
     titlenice( 'Sense Images - Fully Sampled' );
-    saveas( fullySampledSenseReconFig, [outDir, '/recon_fullySampledSenseImages.png'] );
+    saveas( fullySampledSenseReconFig, [datacaseDir, '/recon_fullySampledSenseImages.png'] );
     close( fullySampledSenseReconFig );
 
-    [ Nx, Ny, nSlices, nCoils ] = size( sub_kData );
-    sMask = [ Nx Ny ];
-    if sampleFraction == 1
-      vdMask = ones( sMask );
-    else
-      vdMask = vdSampleMask( sMask, vdSigma, sampleFraction*prod(sMask) );
-    end
-    maskFig = figure;  imshowscale( vdMask, 5 )
-    saveas( maskFig, [outDir, '/sampleMask.png'] );
-    close( maskFig );
+    fullySampledSakeL1EspiritRecon = sakeRecon2D( sub_kData, 'type', 'espiritL1' );
+    sake_ReconFig_fullySampledFig = figure;  imshowscale( abs( fullySampledSakeL1EspiritRecon ), 5 );
+    title('sake L1-ESPIRiT Recon');
+    saveas( sake_ReconFig_fullySampledFig, [datacaseDir, '/recon_sakeEspiritL1_fullySampled.png'] );
+    close( sake_ReconFig_fullySampledFig );
+    printMetrics( logFile, datacase, 1.0, numel(mask), fullySampledSakeL1EspiritRecon, ...
+      'sakeL1EspiritRecon', ssqRecon_fullySampled );
 
-    kData = sub_kData;
-    for slice=1:nSlices
-      for coil=1:nCoils
-        kData(:,:,slice,coil) = vdMask .* sub_kData(:,:,slice,coil);
+    nSamples = sum( mask(:) );
+    parfor sampleFractionIndx = 1 : numel( sampleFractions )
+      sampleFraction = sampleFractions( sampleFractionIndx );
+      lambda_x = lambda_xs( sampleFractionIndx );
+      lambda_s = lambda_ss( sampleFractionIndx );
+      lambda_h = lambda_hs( sampleFractionIndx );
+
+      outDir = [ datacaseDir, '/', num2str(sampleFraction,'%3.2f') ];
+      if ~exist( outDir, 'dir' ), mkdir( outDir ); end
+
+      [ Nx, Ny, nSlices, nCoils ] = size( sub_kData );
+      sMask = [ Nx Ny ];
+      if sampleFraction == 1
+        vdMask = ones( sMask );
+      else
+        vdMask = vdSampleMask( sMask, vdSigma*max(Nx,Ny), ...
+          round( sampleFraction*prod(sMask) ) );
       end
+      kMask = sum( sum( abs( sub_kData ), 3), 4 ) ~= 0;
+      vdMask = vdMask & kMask;
+      maskFig = figure;  imshowscale( vdMask, 5 );
+      saveas( maskFig, [outDir, '/sampleMask.png'] );
+      close( maskFig );
+
+      kData = sub_kData;
+      for slice=1:nSlices
+        for coil=1:nCoils
+          kData(:,:,slice,coil) = vdMask .* sub_kData(:,:,slice,coil);
+        end
+      end
+
+      ssqRecon = mri_ssqRecon( kData );
+      undersampledSsqFig = figure;
+      imshowscale( abs( ssqRecon ), 5 );  titlenice( 'Undersampled ssqRecon' );
+      saveas( undersampledSsqFig, [outDir, '/recon_ssq.png'] );
+      close( undersampledSsqFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, ssqRecon, 'ssq', ...
+        ssqRecon_fullySampled );
+
+      [mccsRecon,mccsMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, lambda_h, ...
+        'kcf', kcf, 'maxOuterIter', maxOuterIter, 'noiseCov', noiseCov, ...
+        'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, ...
+        'outDir', [ outDir, '/mccs/' ] );
+      mccsReconFig = figure; imshowscale( abs(mccsRecon), 5 );  title('mccs Recon');
+      saveas( mccsReconFig, [outDir, '/recon_mccs.png'] );
+      close( mccsReconFig );
+      mapsFig = figure;  showImageCube( abs( mccsMaps ), 5 );
+      saveas( mapsFig, [outDir, '/senseMaps_mccs.png'] );
+      close( mapsFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, mccsRecon, 'mccsRecon', ...
+        ssqRecon_fullySampled );
+  
+      [csSenseRecon,senseMaps] = mri_csSenseRecon( kData, 100 * lambda_x, ...
+        'verbose', verbose );
+      %if ~exist( senseMapFile, 'file' ), save( senseMapFile, 'senseMaps' ); end
+      csSenseFig = figure; imshowscale( abs(csSenseRecon), 5 );  title('csSense Recon');
+      saveas( csSenseFig, [outDir, '/recon_csSense.png'] );
+      close( csSenseFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, csSenseRecon, 'csSense', ...
+        ssqRecon_fullySampled );
+
+      nnSenseRecon = mri_nnSenseRecon( kData, lambda_x*1000, 'senseMaps', senseMaps, ...
+        'verbose', verbose );
+      nnSenseFig = figure; imshowscale( abs(nnSenseRecon), 5 );  title('nnSense Recon');
+      saveas( nnSenseFig, [outDir, '/recon_nnSense.png'] );
+      close( nnSenseFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, nnSenseRecon, 'nnSense', ...
+        ssqRecon_fullySampled );
+  
+      [iSENSEnn_Recon,iSENSEnn_Maps] = mri_iSENSEnn( kData, labmda_iSENSEnn_x, labmda_iSENSEnn_s, ...
+        'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, 'outDir', [ outDir, '/iSENSEnn/' ] );   %#ok<ASGLU>
+      iSENSEnn_ReconFig = figure; imshowscale( abs(iSENSEnn_Recon), 5 );  title('mccs Recon');
+      saveas( iSENSEnn_ReconFig, [outDir, '/recon_iSENSEnn.png'] );
+      close( iSENSEnn_ReconFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, iSENSEnn_Recon, 'iSENSEnn_Recon', ...
+        ssqRecon_fullySampled );
+
+      sakeEspiritRecon = sakeRecon2D( kData, 'type', 'espirit' );
+      sake_ReconFig = figure;  imshowscale( abs( sakeEspiritRecon ), 5 );  title('sake ESPIRiT Recon');
+      saveas( sake_ReconFig, [outDir, '/recon_sakeEspirit.png'] );
+      close( sake_ReconFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, sakeEspiritRecon, 'sakeEspiritRecon', ...
+        ssqRecon_fullySampled);
+
+      sakeL1EspiritRecon = sakeRecon2D( kData, 'type', 'espiritL1' );
+      sake_ReconFig = figure;  imshowscale( abs( sakeL1EspiritRecon ), 5 );  title('sake L1-ESPIRiT Recon');
+      saveas( sake_ReconFig, [outDir, '/recon_sakeEspiritL1.png'] );
+      close( sake_ReconFig );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, sakeL1EspiritRecon, 'sakeL1EspiritRecon', ...
+        ssqRecon_fullySampled);
     end
-
-    ssqRecon = mri_ssqRecon( kData );
-    undersampledSsqFig = figure;
-    imshowscale( abs( ssqRecon ), 5 );  titlenice( 'Undersampled ssqRecon' );
-    saveas( undersampledSsqFig, [outDir, '/recon_ssq.png'] );
-    close( undersampledSsqFig );
-
-    [csSenseRecon,senseMaps] = mri_csSenseRecon( kData, 100 * lambda_x, ...
-      'verbose', verbose );
-    if ~exist( senseMapFile, 'file' ), save( senseMapFile, 'senseMaps' ); end
-    csSenseFig = figure; imshowscale( abs(csSenseRecon), 5 );  title('csSense Recon');
-    saveas( csSenseFig, [outDir, '/recon_csSense.png'] );
-    close( csSenseFig );
-
-    nnSenseRecon = mri_nnSenseRecon( kData, lambda_x*1000, 'senseMaps', senseMaps, ...
-      'verbose', verbose );
-    nnSenseFig = figure; imshowscale( abs(nnSenseRecon), 5 );  title('nnSense Recon');
-    saveas( nnSenseFig, [outDir, '/recon_nnSense.png'] );
-    close( nnSenseFig );
-
-    [iSENSEnn_Recon,iSENSEnn_Maps] = mri_iSENSEnn( kData, labmda_iSENSEnn_x, labmda_iSENSEnn_s, ...
-      'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, 'outDir', [ outDir, '/iSENSEnn/' ] );   %#ok<ASGLU>
-    iSENSEnn_ReconFig = figure; imshowscale( abs(iSENSEnn_Recon), 5 );  title('mccs Recon');
-    saveas( iSENSEnn_ReconFig, [outDir, '/recon_iSENSEnn.png'] );
-    close( iSENSEnn_ReconFig );
-
-    sakeRecon = sakeRecon2D( kData );
-    sake_ReconFig = figure;  imshowscale( abs( sakeRecon ), 5 );  title('sake Recon');
-    saveas( sake_ReconFig, [outDir, '/recon_sake.png'] );
-    close( sake_ReconFig );
-
-    [mccsRecon,mccsMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, ...
-      'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, 'outDir', [ outDir, '/mccs/' ] );
-    mccsReconFig = figure; imshowscale( abs(mccsRecon), 5 );  title('mccs Recon');
-    saveas( mccsReconFig, [outDir, '/recon_mccs.png'] );
-    close( mccsReconFig );
-
-    %recons = cat( 3, ...
-    %  scaleImg( ssqRecon_fullySampled / max( ssqRecon_fullySampled(:) ), [0 1] ), ...
-    %  scaleImg( ssqRecon / max( ssqRecon(:) ), [0 1] ), ...
-    %  scaleImg( senseRecon / max( senseRecon(:) ), [0 1] ), ...
-    %  scaleImg( mccsRecon / max( mccsRecon(:) ), [0 1] ) );
-
-    %figure; showImageCube( senseMaps, 'border', 1, 'borderValue', 'max' );  titlenice( 'sense maps' )
-    %figure; showImageCube( mccsMaps, 'border', 1, 'borderValue', 'max' );  titlenice( 'mccs maps' );
-    %figure; showImageCube( recons, 5, 'border', 1, 'borderValue', 'max', 'nImgsPerRow', 4 );
   end
 end
-
-
-
 
