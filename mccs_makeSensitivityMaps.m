4d6b28926bee6d21bda77823ec70f08acde9d467
Nicholas Dwork <ndwork@hotmail.com>
2019-11-26 17:21:53 -0800

    Now impose L2 regularization on high frequency components rather than hard cutoff.

--- mccs_makeSensitivityMaps.m
+++ mccs_makeSensitivityMaps.m
@@ -1,5 +1,5 @@
 
-function senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, varargin )
+function senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, lambda_h, varargin )
   % senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf [, ...
   %   'maxIterOpt', maxIterOpt ] )
   %
@@ -11,7 +11,7 @@ function senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, vara
   % cf - the cutoff spatial frequency
   %
   % Optional Inputs:
-  % invN - the inverse covariance matrix specifying how the noise between coils is correlated
+  % noiseCov - the nCoils x nCoils noise covariance matrix
   %
   % Outputs:
   % senseMaps - the sensitivity maps
@@ -30,36 +30,52 @@ function senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, vara
   p.addRequired( 'kcf', @ispositive );
   p.addParameter( 'doCheckAdjoint', false, @islogical );
   p.addParameter( 'initialGuess', [], @isnumeric );
-  p.addParameter( 'invN', [], @isnumeric );
   p.addParameter( 'maxIterOpt', 100, @ispositive );
+  p.addParameter( 'noiseCov', [], @isnumeric );
   p.addParameter( 'verbose', false, @islogical );
   p.parse( kData, kcf, varargin{:} );
   doCheckAdjoint = p.Results.doCheckAdjoint;
   initialGuess = p.Results.initialGuess;
-  invN = p.Results.invN;
   maxIterOpt = p.Results.maxIterOpt;
+  noiseCov = p.Results.noiseCov;
   verbose = p.Results.verbose;
 
-  [ Nx, Ny, nSlices, nCoils ] = size( kData );
-  ks = size2fftCoordinates( [ Nx Ny ] );
+  [ Ny, Nx, nSlices, nCoils ] = size( kData );
+  nPix = Ny * Nx;
+  ks = size2fftCoordinates( [ Ny Nx ] );
   [ kxs, kys ] = meshgrid( ks{1}, ks{2} );
   kMag = sqrt( kxs .* kxs + kys .* kys );
   kBwMask = repmat( kMag < kcf, [ 1 1 nCoils ] );
   kMask = squeeze( sum( abs(kData), 3 ) ) ~= 0;
-  nPix = Ny * Nx;
+  nb = sum( kMask(:) );
+  kMaskCoil = sum( abs( kMask ), 3 ) > 0;
+  nbPerCoil = sum( kMaskCoil(:) );
 
-  function out = Z( x )
-    out = zeros( Nx, Ny, nCoils );
-    out( kBwMask == 1 ) = x(:);
+  if numel( noiseCov ) > 0
+    invNoiseCov = inv( noiseCov );
+    [~,s,~] = svd( invNoiseCov, 'econ' );
+    invNoiseCov = invNoiseCov ./ s(1);
+    L = chol( invNoiseCov, 'lower' );
   end
+  function out = applyL( in, type )
+    if numel( noiseCov ) == 0, out = in; return; end
 
-  function out = Zadj( x )
-    out = x( kBwMask == 1 );
+    % Assumes last dimension is coil dimension
+    if nargin < 2, type = 'notransp'; end
+
+    sIn = size( in );
+    reshaped = reshape( in, [ prod( sIn(1:end-1) ) sIn(end) ] );
+    if strcmp( type, 'notransp' )
+      out = transpose( L * transpose( reshaped ) );
+    else
+      out = transpose( L' * transpose( reshaped ) );
+    end
+    out = reshape( out, sIn );
   end
 
   function out = F( x )
-    tmp = ifftshift( ifftshift( x, 1 ), 2 );   % iShiftedX
-    out = 1 / sqrt( nPix ) * fftc( fftc( tmp, [], 1 ), [], 2 );
+    iShiftedX = ifftshift( ifftshift( x, 1 ), 2 );
+    out = 1 / sqrt( nPix ) * fftc( fftc( iShiftedX, [], 1 ), [], 2 );
   end
 
   function out = Fadj( y )
@@ -67,117 +83,128 @@ function senseMaps = mccs_makeSensitivityMaps( recon, kData, kcf, lambda_s, vara
     out = fftshift( fftshift( out, 1 ), 2 );
   end
 
-  nb = sum( kMask(:) );
+  function out = applyA1( in, type )
+    if strcmp( type, 'notransp' )
+      Xin = bsxfun( @times, in, recon );
+      FXin = F( Xin );
+      DFXin = FXin( kMask == 1 );
+      if numel( noiseCov ) > 0
+        LStarDFXin = applyL( reshape( DFXin, [ nbPerCoil nCoils ] ), 'transp' );
+      else
+        LStarDFXin = DFXin;
+      end
+      out = LStarDFXin(:);
+    else
+      if numel( noiseCov ) > 0
+        Lin = applyL( reshape( in, [ nbPerCoil nCoils ] ), 'notransp' );
+      else
+        Lin = in;
+      end
+      DTLin = zeros( Ny, Nx, nCoils );
+      DTLin( kMask == 1 ) = Lin(:);
+      FadjDTLin = Fadj( DTLin );
+      out = bsxfun( @times, FadjDTLin, conj( recon ) );
+    end
+  end
+
+  function out = applyA2( in, type )
+    if strcmp( type, 'notransp' )
+      s = reshape( in, [ Ny Nx nCoils ] );
+      Fs = F( s );
+      DbwFs = Fs( kBwMask == 0 );
+      out = sqrt( lambda_h ) * DbwFs;
+    else
+      tmp = zeros( Ny, Nx, nCoils );
+      tmp( kBwMask == 0 ) = in;
+      DbwIn = sqrt( lambda_h ) * tmp;
+      out = Fadj( DbwIn );
+    end
+  end
+
+  nHighFreq = sum( kBwMask(:) == 0 );
   function out = applyA( in, type )
     if nargin < 2, type = 'notransp'; end
 
     if strcmp( type, 'notransp' )
-      Zs = Z( in );
-      FadjZs = Fadj( Zs );
-      XFadjZs = bsxfun( @times, FadjZs, recon );
-      FXFadjZs = F( XFadjZs );
-      DFXFadjZs = FXFadjZs( kMask == 1 );
-      out = [ DFXFadjZs(:); FadjZs(:); FadjZs(:); ];
+      Ain1 = applyA1( reshape( in, [ Ny Nx nCoils ] ), type );
+      Ain2 = applyA2( reshape( in, [ Ny Nx nCoils ] ), type );
+      out = [ Ain1(:); Ain2(:); in(:); ];
 
     else
+      y1 = in( 1 : nb );
+      y2 = in( nb + 1 : nb + nHighFreq );
+      y3 = in( nb + nHighFreq + 1 : nb + nHighFreq + nPix*nCoils );
 
-      y1 = zeros( Nx, Ny, nCoils );
-      y1( kMask == 1 ) = in( 1 : nb );
-      Fadjy = Fadj( y1 );
-      XadjFadjy = bsxfun( @times, Fadjy, conj( recon ) );
-      FXadjFadjy = F( XadjFadjy );
-      ZadjFXadjFadjy = Zadj( FXadjFadjy );
-      out1 = ZadjFXadjFadjy(:);
+      AT1 = applyA1( y1, type );
+      AT2 = applyA2( y2, type );
 
-      y2 = reshape( in( nb + 1 : nb + nPix*nCoils ), [ Nx, Ny, nCoils ] );
-      Fy2 = F( y2 );
-      ZadjFy2 = Zadj( Fy2 );
-      out2 = ZadjFy2(:);
-
-      y3 = reshape( in( nb + nPix*nCoils + 1 : end ), [ Nx, Ny, nCoils ] );
-      Fy3 = F( y3 );
-      ZadjFy3 = Zadj( Fy3 );
-      out3 = ZadjFy3(:);
-
-      out = out1 + out2 + out3;
+      out = AT1(:) + AT2(:) + y3(:);
     end
   end
 
   if doCheckAdjoint
     if ~checkAdjoint( kBwMask, @F, @Fadj ), error('checkAdjoint failed'); end
-    if ~checkAdjoint( kBwMask(kBwMask==1), @Z, @Zadj ), error('checkAdjoint failed'); end
-    if ~checkAdjoint( kBwMask(kBwMask==1), @applyA ), error('checkAdjoint failed'); end
+    if ~checkAdjoint( kBwMask, @applyA1 ), error('checkAdjoint failed'); end
+    if ~checkAdjoint( kBwMask, @applyA2 ), error('checkAdjoint failed'); end
+    if ~checkAdjoint( kBwMask, @applyA ), error('checkAdjoint failed'); end
   end
 
-  fftSenseMaps0 = ones( sum( kBwMask(:) ), nSlices );
-  if numel( initialGuess ) > 0
-    fftInitialGuess = F( initialGuess );
-    for sliceIndx = 1 : nSlices
-      tmp = squeeze( fftInitialGuess( :, :, sliceIndx, : ) );
-      fftSenseMaps0(:,sliceIndx) = tmp( kBwMask==1 );
-    end
-  end
-
-  f = @(y) 0;
-  proxf = @(x,t) x;
-
-  function out = proxg1Conj( y, t )
-    out = ( y - t * b ) ./ ( t + 1 );
-  end
-
-  function out = proxg2Conj( y, t )
-    out = softThresh( y, t );
-  end
-
-  function out = proxg3Conj( y, lambda )
-    S = reshape( y, [ nPix, nCoils ] );
-    out = proxConjNucNorm( S, lambda );
-    out = out( : );
-  end
-
-  proxgConj = @(y,t) [ proxg1Conj( y( 1 : nb ), t ); ...
-                       proxg2Conj( y( nb+1 : nb+nPix*nCoils ), t ); ...
-                       proxg3Conj( y( nb+nPix*nCoils+1 : end ), lambda_s ); ];
-
+  f = @(s) lambda_s * nucNorm( reshape( s, [ nPix nCoils ] ) );
   function out = g( y )
-    y1 = y(1:nb);
-    %y2 = y( nb+1 : nb+nPix*nCoils );  % We will not include indiator function
-    y3 = y( nb+nPix*nCoils+1 : end );
-    S = reshape( y3, [ nPix, nCoils ] );
-    out = 0.5 * norm( y1 - b ).^2 + lambda_s * nucNorm( S );
+    y1 = y( 1 : nb );
+    y2 = y( nb + 1 : nb + nHighFreq );
+    y3 = y( nb + nHighFreq + 1 : nb + nHighFreq + nPix*nCoils );  %#ok<NASGU>
+
+    out1 = 0.5 * norm( y1 - LStarb(:) )^2;
+    out2 = 0.5 * norm( y2 )^2;
+    % I will not include the indicator function
+    % I may want to indicate how much the constraint is violated.
+
+    out = out1 + out2;
   end
 
-  senseMaps = zeros( Nx, Ny, nSlices, nCoils );
+  function out = proxf( y, t )
+    out = proxNucNorm( reshape( y, [ nPix nCoils ] ), t * lambda_s );
+    out = out(:);
+  end
+
+  function out = proxgConj( y, t )
+    y1 = y( 1 : nb );
+    y2 = y( nb + 1 : nb + nHighFreq );
+    y3 = y( nb + nHighFreq + 1 : nb + nHighFreq + nPix*nCoils );
+
+    out1 = ( y1 - t * LStarb(:) ) ./ ( t + 1 );
+    out2 = y2 ./ ( t + 1 );
+    out3 = softThresh( y3, t );
+
+    out = [ out1(:); out2(:); out3(:); ];
+  end
+
+
+  if numel( initialGuess ) == 0
+    initialGuess = ones( [ Ny Nx nSlices nCoils ] );
+  end
+
+  senseMaps = initialGuess;
   for sliceIndx = 1 : nSlices
-
     sliceKData = squeeze( kData( :, :, sliceIndx, : ) );
-    b = sliceKData( kMask == 1 );
+    sliceGuess = squeeze( initialGuess( :, :, sliceIndx, : ) );
 
-    fftSliceSenseMaps0 = fftSenseMaps0( :, sliceIndx );
+    bKData = reshape( sliceKData, [nPix nCoils] );
+    b = bKData( kMaskCoil(:) == 1, : );
+    LStarb = applyL( b, 'transp' );
 
     beta = 1;
-    tau = 1d2;  % Works well
-    theta = 1.9;
     if verbose == true
-      
-      normA = powerIteration( @applyA, fftSliceSenseMaps0(:) );
-      %[x,objValues] = chambollePock( fftSliceSenseMaps0(:), proxf, proxgConj, tau, ...
-      %  'A', @applyA, 'normA', normA, 'N', maxIterOpt, 'verbose', verbose, ...
-      %  'theta', theta, 'f', f, 'g', @g );   %#ok<ASGLU>
-      [x,objValues] = chambollePockWLS( fftSliceSenseMaps0(:), proxf, proxgConj, ...
+      [x,objValues] = chambollePockWLS( sliceGuess(:), @proxf, @proxgConj, ...
         'A', @applyA, 'beta', beta, 'N', maxIterOpt, 'verbose', verbose, ...
         'doCheckAdjoint', doCheckAdjoint, 'f', f, 'g', @g );   %#ok<ASGLU>
-
     else
-
-      x = chambollePock( fftSliceSenseMaps0(:), proxf, proxgConj, tau, ...
-        'A', @applyA, 'normA', normA, 'N', maxIterOpt, 'theta', theta );
-      %x = chambollePockWLS( fftSliceSenseMaps0(:), proxf, proxgConj, 'A', @applyA, ...
-      %  'beta', beta, 'N', maxIterOpt, 'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint );
-
+      x = chambollePockWLS( sliceGuess(:), @proxf, @proxgConj, 'A', @applyA, ...
+        'beta', beta, 'N', maxIterOpt, 'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint );
     end
 
-    sliceSenseMap = reshape( Fadj( Z( x ) ), [ Nx Ny nCoils ] );
+    sliceSenseMap = reshape( x, [ Nx Ny nCoils ] );
     senseMaps(:,:,sliceIndx,:) = sliceSenseMap;
 
     %Ax = applyA( x(:), 'notransp' );
