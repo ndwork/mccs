bb0f60b39d938797eaef12bc9565255b9c18800d
Nicholas Dwork <ndwork@hotmail.com>
2019-11-26 17:19:20 -0800

    Major code cleanup on run_mri_mccsRecon.

--- run_mri_mccsRecon.m
+++ run_mri_mccsRecon.m
@@ -2,7 +2,7 @@
 function run_mri_mccsRecon
   close all; clear; rng(1);
 
-  %datacases = [ 1 7 3 2 5 8 6 ];
+  %datacases = [ 3 1 7 2 5 8 6 ];
 datacases = 3;
 
   doCheckAdjoint = false;
@@ -13,7 +13,8 @@ vdSigma = 0.3;
   maxOuterIter = 100;
   useNoiseCov = true;
 
-  sampleFractions = [ 0.1 0.15 0.2 0.25 0.3 0.4 ];
+  %sampleFractions = [ 0.1 0.15 0.2 0.25 0.3 0.4 ];
+sampleFractions = 0.3;
 
   %lambda_x = 5d-4;  % 1d-5 works well without noise covariance
   %lambda_s = 1d-4;
@@ -60,7 +61,7 @@ vdSigma = 0.3;
     mask = mri_makeIntensityMask( sub_kData );
     fullySampledSenseMapsMatFile = ['fullySampledSenseMaps_', indx2str(datacase,99), '.mat' ];
     if exist( fullySampledSenseMapsMatFile, 'file' )
-      load( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );  %#ok<LOAD>
+      load( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );
     else
       fullySampledSenseMaps = mri_makeSensitivityMaps( sub_kData, 'mask', mask, 'verbose', verbose );
       save( fullySampledSenseMapsMatFile, 'fullySampledSenseMaps' );
@@ -79,27 +80,16 @@ vdSigma = 0.3;
     end
 
     ssqRecon_fullySampled = mri_ssqRecon( sub_kData, 'multiSlice', true);
-    ssqRecon_fullySampledFig = figure;  imshowscale( abs( ssqRecon_fullySampled ), 5 );
-    saveas( ssqRecon_fullySampledFig, [datacaseDir, '/recon_ssqFullySampled.png'] );
-    close( ssqRecon_fullySampledFig );
-    printMetrics( logFile, datacase, 1.0, numel(mask), ssqRecon_fullySampled, 'ssq Fully Sampled' );
-
-    fullySampledSenseRecons = bsxfun( @times, fullySampledSenseMaps, ssqRecon_fullySampled );
-    fullySampledSenseReconFig = figure;  showImageCube( abs(fullySampledSenseRecons), 5 );
-    titlenice( 'Sense Images - Fully Sampled' );
-    saveas( fullySampledSenseReconFig, [datacaseDir, '/recon_fullySampledSenseImages.png'] );
-    close( fullySampledSenseReconFig );
+    printMetrics( logFile, datacase, 1.0, numel(mask), ssqRecon_fullySampled, ...
+      'ssqFullySampled', datacaseDir, 'senseMaps', fullySampledSenseMaps );
 
     fullySampledSakeL1EspiritRecon = sakeRecon2D( sub_kData, 'type', 'espiritL1' );
-    sake_ReconFig_fullySampledFig = figure;  imshowscale( abs( fullySampledSakeL1EspiritRecon ), 5 );
-    title('sake L1-ESPIRiT Recon');
-    saveas( sake_ReconFig_fullySampledFig, [datacaseDir, '/recon_sakeEspiritL1_fullySampled.png'] );
-    close( sake_ReconFig_fullySampledFig );
     printMetrics( logFile, datacase, 1.0, numel(mask), fullySampledSakeL1EspiritRecon, ...
-      'sakeL1EspiritRecon', ssqRecon_fullySampled );
+      'sakeL1EspiritRecon', datacaseDir, ssqRecon_fullySampled );
 
     nSamples = sum( mask(:) );
-    parfor sampleFractionIndx = 1 : numel( sampleFractions )
+    %parfor sampleFractionIndx = 1 : numel( sampleFractions )
+for sampleFractionIndx = 1 : numel( sampleFractions )
       sampleFraction = sampleFractions( sampleFractionIndx );
       lambda_x = lambda_xs( sampleFractionIndx );
       lambda_s = lambda_ss( sampleFractionIndx );
@@ -113,8 +103,8 @@ vdSigma = 0.3;
       if sampleFraction == 1
         vdMask = ones( sMask );
       else
-        vdMask = vdSampleMask( sMask, vdSigma*max(Nx,Ny), ...
-          round( sampleFraction*prod(sMask) ) );
+        sampleGoal = round( sampleFraction*prod(sMask) );
+        vdMask = vdSampleMask( sMask, vdSigma*max(Nx,Ny), sampleGoal );
       end
       kMask = sum( sum( abs( sub_kData ), 3), 4 ) ~= 0;
       vdMask = vdMask & kMask;
@@ -130,64 +120,36 @@ vdSigma = 0.3;
       end
 
       ssqRecon = mri_ssqRecon( kData );
-      undersampledSsqFig = figure;
-      imshowscale( abs( ssqRecon ), 5 );  titlenice( 'Undersampled ssqRecon' );
-      saveas( undersampledSsqFig, [outDir, '/recon_ssq.png'] );
-      close( undersampledSsqFig );
-      printMetrics( logFile, datacase, sampleFraction, nSamples, ssqRecon, 'ssq', ...
-        ssqRecon_fullySampled );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, ssqRecon, 'undersampledSsq', ...
+        outDir, ssqRecon_fullySampled );
 
-      [mccsRecon,mccsMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, lambda_h, ...
-        'kcf', kcf, 'maxOuterIter', maxOuterIter, 'noiseCov', noiseCov, ...
-        'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, ...
-        'outDir', [ outDir, '/mccs/' ] );
-      mccsReconFig = figure; imshowscale( abs(mccsRecon), 5 );  title('mccs Recon');
-      saveas( mccsReconFig, [outDir, '/recon_mccs.png'] );
-      close( mccsReconFig );
-      mapsFig = figure;  showImageCube( abs( mccsMaps ), 5 );
-      saveas( mapsFig, [outDir, '/senseMaps_mccs.png'] );
-      close( mapsFig );
+      [mccsRecon,mccsMaps] = mri_mccsRecon( kData, lambda_x, lambda_s, lambda_h, 'kcf', kcf, ...
+        'maxOuterIter', maxOuterIter, 'noiseCov', noiseCov, 'verbose', verbose, ...
+        'doCheckAdjoint', doCheckAdjoint, 'outDir', [ outDir, '/mccs/' ] );
       printMetrics( logFile, datacase, sampleFraction, nSamples, mccsRecon, 'mccsRecon', ...
-        ssqRecon_fullySampled );
-  
-      [csSenseRecon,senseMaps] = mri_csSenseRecon( kData, 100 * lambda_x, ...
-        'verbose', verbose );
-      %if ~exist( senseMapFile, 'file' ), save( senseMapFile, 'senseMaps' ); end
-      csSenseFig = figure; imshowscale( abs(csSenseRecon), 5 );  title('csSense Recon');
-      saveas( csSenseFig, [outDir, '/recon_csSense.png'] );
-      close( csSenseFig );
-      printMetrics( logFile, datacase, sampleFraction, nSamples, csSenseRecon, 'csSense', ...
-        ssqRecon_fullySampled );
+        outDir, ssqRecon_fullySampled, 'senseMaps', mccsMaps );
 
-      nnSenseRecon = mri_nnSenseRecon( kData, lambda_x*1000, 'senseMaps', senseMaps, ...
+      [csSenseRecon,senseMaps] = mri_csSenseRecon( kData, labmda_iSENSEnn_x, 'verbose', verbose );
+      printMetrics( logFile, datacase, sampleFraction, nSamples, csSenseRecon, 'csSense', ...
+        outDir, ssqRecon_fullySampled, 'senseMaps', senseMaps );
+
+      [nnSenseRecon,nnMaps] = mri_nnSenseRecon( kData, labmda_iSENSEnn_x, 'senseMaps', senseMaps, ...
         'verbose', verbose );
-      nnSenseFig = figure; imshowscale( abs(nnSenseRecon), 5 );  title('nnSense Recon');
-      saveas( nnSenseFig, [outDir, '/recon_nnSense.png'] );
-      close( nnSenseFig );
       printMetrics( logFile, datacase, sampleFraction, nSamples, nnSenseRecon, 'nnSense', ...
-        ssqRecon_fullySampled );
-  
+        outDir, ssqRecon_fullySampled, 'senseMaps', nnMaps );
+
       [iSENSEnn_Recon,iSENSEnn_Maps] = mri_iSENSEnn( kData, labmda_iSENSEnn_x, labmda_iSENSEnn_s, ...
         'verbose', verbose, 'doCheckAdjoint', doCheckAdjoint, 'outDir', [ outDir, '/iSENSEnn/' ] );   %#ok<ASGLU>
-      iSENSEnn_ReconFig = figure; imshowscale( abs(iSENSEnn_Recon), 5 );  title('mccs Recon');
-      saveas( iSENSEnn_ReconFig, [outDir, '/recon_iSENSEnn.png'] );
-      close( iSENSEnn_ReconFig );
       printMetrics( logFile, datacase, sampleFraction, nSamples, iSENSEnn_Recon, 'iSENSEnn_Recon', ...
-        ssqRecon_fullySampled );
+        outDir, ssqRecon_fullySampled, 'senseMaps', iSENSEnn_Maps );
 
       sakeEspiritRecon = sakeRecon2D( kData, 'type', 'espirit' );
-      sake_ReconFig = figure;  imshowscale( abs( sakeEspiritRecon ), 5 );  title('sake ESPIRiT Recon');
-      saveas( sake_ReconFig, [outDir, '/recon_sakeEspirit.png'] );
-      close( sake_ReconFig );
       printMetrics( logFile, datacase, sampleFraction, nSamples, sakeEspiritRecon, 'sakeEspiritRecon', ...
-        ssqRecon_fullySampled);
+        outDir, ssqRecon_fullySampled);
 
       sakeL1EspiritRecon = sakeRecon2D( kData, 'type', 'espiritL1' );
-      sake_ReconFig = figure;  imshowscale( abs( sakeL1EspiritRecon ), 5 );  title('sake L1-ESPIRiT Recon');
-      saveas( sake_ReconFig, [outDir, '/recon_sakeEspiritL1.png'] );
-      close( sake_ReconFig );
       printMetrics( logFile, datacase, sampleFraction, nSamples, sakeL1EspiritRecon, 'sakeL1EspiritRecon', ...
-        ssqRecon_fullySampled);
+        outDir, ssqRecon_fullySampled);
     end
   end
 end
