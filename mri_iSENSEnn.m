b736c423c8c0179d01897af9992537b22b194e38
Nicholas Dwork <ndwork@hotmail.com>
2020-11-30 12:36:14 -0800

    Replaced fftc with fft.

--- mri_iSENSEnn.m
+++ mri_iSENSEnn.m
@@ -94,7 +94,7 @@ function senseMaps = iSENSEnn_makeInitialSensitivityMap( kData )
   sKData = size( kData );
   nSamples = sKData(1) * sKData(2);
 
-  shiftedRecons = sqrt( nSamples ) * ifftc( ifftc( kData, 1 ), 2 );
+  shiftedRecons = sqrt( nSamples ) * ifft( ifft( kData, [], 1 ), [], 2 );
   coilRecons = fftshift( fftshift( shiftedRecons, 1 ), 2 );
 
   ssqRecon = norms( coilRecons, 2, 4 );
